4d9619533d3add89163d9083c6db0b79f83f7dde
Faruq2014 <mjmolla@gmail.com>
2022-03-09 20:17:24 -0500

    adding first featue branch

---
+++ src/main/java/gitMergeFirstFeatue/FirstFeature.java
@@ -0,0 +1,11 @@
+package gitMergeFirstFeatue;
+
+public class FirstFeature {
+
+	public static void main(String[] args) {
+		String fisrtFeature="First Feature Branch";
+		System.out.println(fisrtFeature);
+
+	}
+
+}
