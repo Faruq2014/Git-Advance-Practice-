e08ada09f396ddbc8fea48dd2933cd215d907273
Faruq2014 <mjmolla@gmail.com>
2022-03-09 20:10:00 -0500

    creating develop branch

---
+++ src/test/java/gitMergeDevelop/HelloDevelop.java
@@ -0,0 +1,10 @@
+package gitMergeDevelop;
+
+public class HelloDevelop {
+
+	public static void main(String[] args) {
+		String develop="Hello Development Branch";
+		System.out.println(develop);
+	}
+
+}
